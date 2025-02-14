74f02040b0e898435baac5155b8cf53d5dbc8f30
Kylo33 <56988649+Kylo33@users.noreply.github.com>
2025-02-14 23:08:21 +0000

    Remove environment variables due to Expo bug (web)

--- firebase.js
+++ firebase.js
@@ -1,19 +1,21 @@
 // // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, setPersistence, browserLocalPersistence, initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import { getFirestore } from "firebase/firestore";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeApp } from "firebase/app";
+import { initializeAuth } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
+
+//@ts-ignore
+import { getReactNativePersistence } from '@firebase/auth/dist/rn/index.js';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID
+  apiKey: "AIzaSyDyhJ0oMOY46Z8Zay5kjchCTJkFdQUf3fI",
+  authDomain: "lets-go-e0eb8.firebaseapp.com",
+  projectId: "lets-go-e0eb8",
+  storageBucket: "lets-go-e0eb8.firebasestorage.app",
+  messagingSenderId: "6799887660",
+  appId: "1:6799887660:web:d3ab80f0c7f18e0c467089",
+  measurementId: "G-HK04KV03NM"
 };
 
 // Initialize Firebase
