6f6d48f015535484cf22d5c54c43501bf3dc488a
Kylo33 <56988649+Kylo33@users.noreply.github.com>
2025-03-08 00:13:51 +0000

    Add icons, remove labels

--- app/main/trip/[id]/_layout.tsx
+++ app/main/trip/[id]/_layout.tsx
@@ -7,11 +7,11 @@ import React, { useEffect, useState } from 'react';
 import { Button, Dialog, Icon, IconButton, PaperProvider, Portal, useTheme } from 'react-native-paper';
 import { withLayoutContext } from "expo-router";
 import {
-  createMaterialTopTabNavigator
+  createMaterialTopTabNavigator,
+  MaterialTopTabNavigationEventMap,
+  MaterialTopTabNavigationOptions,
 } from "@react-navigation/material-top-tabs";
-
-const MaterialTopTabs = createMaterialTopTabNavigator();
-const ExpoRouterMaterialTopTabs = withLayoutContext(MaterialTopTabs.Navigator)
+import { TabNavigationState, ParamListBase } from '@react-navigation/native';
 
 export default function TabLayout() {
   const theme = useTheme();
@@ -35,37 +35,35 @@ export default function TabLayout() {
     fetchTrip().catch(console.error);
 }, [id]);
 
+  const MaterialTopTabs = createMaterialTopTabNavigator();
+  const ExpoRouterMaterialTopTabs = withLayoutContext<
+    MaterialTopTabNavigationOptions,
+    typeof MaterialTopTabs.Navigator,
+    TabNavigationState<ParamListBase>,
+    MaterialTopTabNavigationEventMap
+  >(MaterialTopTabs.Navigator);
+
   return (
       <>
         <Stack.Screen options={{ title: trip?.title ?? "Event Details", headerRight: () => <ShareButton /> }} />
         <TripContext.Provider value={trip}>
-          <ExpoRouterMaterialTopTabs />
-          {/* <Tabs screenOptions={{ tabBarActiveTintColor: theme.colors.primary, headerShown: false}}>
-              <Tabs.Screen
-                  name="index"
-                  options={{
-                      title: "Overview",
-                      tabBarIcon: ({color, size}) => <Icon source="home" size={size} color={color}/>,
-                      tabBarLabelStyle: fontObject,
-                  }}
-              />
-              <Tabs.Screen
-                  name="ideas"
-                  options={{
-                      title: "Ideas",
-                      tabBarIcon: ({color, size}) => <Icon source="lightbulb-on" size={size} color={color}/>,
-                      tabBarLabelStyle: fontObject
-                  }}
-              />
-              <Tabs.Screen
-                  name="plan"
-                  options={{
-                      title: "Plan",
-                      tabBarIcon: ({color, size}) => <Icon source="notebook" size={size} color={color}/>,
-                      tabBarLabelStyle: fontObject
-                  }}
-              />
-          </Tabs> */}
+          <ExpoRouterMaterialTopTabs tabBarPosition='bottom'>
+            <ExpoRouterMaterialTopTabs.Screen name='index' options={{
+              tabBarShowLabel: false,
+              tabBarIcon: ({color, size}) => <Icon source="home" size={size} color={color}/>,
+              tabBarLabelStyle: fontObject,
+            }}/>
+            <ExpoRouterMaterialTopTabs.Screen name='ideas' options={{
+              tabBarShowLabel: false,
+              tabBarIcon: ({color, size}) => <Icon source="head-flash" size={size} color={color}/>,
+              tabBarLabelStyle: fontObject,
+            }}/>
+            <ExpoRouterMaterialTopTabs.Screen name='plan' options={{
+              tabBarShowLabel: false,
+              tabBarIcon: ({color, size}) => <Icon source="notebook" size={size} color={color}/>,
+              tabBarLabelStyle: fontObject,
+            }}/>
+          </ExpoRouterMaterialTopTabs>
       </TripContext.Provider>
     </>
   );
